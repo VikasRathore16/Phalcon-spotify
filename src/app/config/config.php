0943297c39d9d0e31d2802bc5516b2bec76b3fa2
Vikas Rathore <vikasrathore@cedcoss.com>
2022-04-11 12:27:56 +0530

    playlist

--- src/app/config/config.php
+++ src/app/config/config.php
@@ -5,7 +5,7 @@ return [
         'version'      => '1.0.0',
         'client_id' => 'f559cc964f2046428482e389e7960d53',
         'client_key' => '2c153c753ecf4fad988c7a95514d7cb4',
-        'bearer_token' => 'BQB_sQp4W1pjaLvYF4L4t8T_Sb8n9Cgbzo9UP_PufvHAlfs6nHWk_ytshNvxUxxauVLdMm0QyG2qzj1sDpZl56TwinHty3q4F2K_rsUkvYWNcBvX84bJcZQxXlyarrUt-1GeZePTm0tggDhOfEvpgMkOf2ssuGKT8QDxpuRGZmglgGtGkJqUb121vUl8-DYPPtudKgZjdrEW5k99'
+        'bearer_token' => 'BQBHwYL2_3LvOlGO6QwD_GoiIj4UWbCunPL5IU5HTDepb17DMtn9rzCju32ZbIQfCxjPQF1IORl4i0aF7m2mQZ-rfsREWTw5pCHztWV2hkOHPximG_88lQZ5yjPF0unUI3LpHggR0tcaD_oFdp3WEV_GzmSGSP9YzE0dJQiOMxHqyRCzjxyb7nyq5LNDoogNNq3li2jYHPy3oQTe'
     ],
     'url' => [
         'base_url'     => 'https://api.spotify.com/v1',
--- src/app/controllers/IndexController.php
+++ src/app/controllers/IndexController.php
@@ -8,44 +8,87 @@ class IndexController extends Controller
 
     public function indexAction()
     {
+
         print_r($this->config->get('app')['bearer_token']);
-        // die();
         $this->view->result = $this->find('/search?q=hello&type=track');
     }
 
     public function searchAction()
     {
-
         $query = $this->request->get('query');
+        $url = '/search?q=' . $query . '&type=';
+        foreach (array_slice($_POST, 1) as $key => $value) {
+            $url .= $value . ',';
+        }
+        $url = rtrim($url, ",");
         $this->session->set('query', $query);
-        $result = $this->find('GET', '/search?q=' . $query . '&type=track,artist');
+        $result = $this->find('GET', $url);
         $myPlaylists = $this->find('GET', '/me/playlists');
         $top_result = $this->topResults($result->tracks['items']);
+        
         $this->view->result = $result;
         $this->view->top_result = $top_result;
         $this->view->myPlaylists = $myPlaylists;
-        echo "<pre>";
-        print_r($result);
-        echo "</pre>";
-        // die();
     }
 
     public function createPlaylistAction()
     {
-        // print_r($this->session);
-        // die();
-        if ($this->request->get('playlist')) {
-            $result = $this->find("POST", '/users/312hyyvvs6hfnvahqxsrq223itvm/playlists', $this->request->get('playlist'));
+        if ($this->request->has('playlist')) {
+            $playlist_body = [
+                "name" => $this->request->get('playlist'),
+                "description" => "New playlist description",
+                "public" => false
+            ];
+
+            $result = $this->find("POST", '/users/312hyyvvs6hfnvahqxsrq223itvm/playlists', $playlist_body);
+
             $this->response->redirect('index/search?query=' . $this->session->get('query') . '&type=track,artist');
         }
     }
 
+    public function deleteFromPlaylistAction()
+    {
+        $song = $this->request->get('song');
+        $playlist_name = $this->request->get('myPlaylist');
+        $playlist_body = [
+            "tracks" => [
+                [
+                    "uri" => $song,
+                ]
+            ],
+        ];
+
+        $result = $this->find("DELETE", '/playlists/' . $playlist_name . '/tracks', $playlist_body);
+        $this->response->redirect('index/myPlaylist?myPlaylist=' . $playlist_name);
+    }
+
+    public function myPlaylistAction()
+    {
+
+        if ($this->request->has('myPlaylist')) {
+            $result = $this->find("GET", '/playlists/' . $this->request->get('myPlaylist'));
+            $this->view->myPlaylist = $result;
+        }
+    }
+
+
     public function likedSongsAction()
     {
-        
+
         echo "<pre>";
         print_r($result);
         echo "</pre>";
+    }
+
+    public function addToPlaylistAction()
+    {
+        $myPlaylists = $this->find('GET', '/me/playlists');
+        $this->view->myPlaylists = $myPlaylists;
+        if ($this->request->has('song') && $this->request->get('myPlaylist')) {
+            $result = $this->find("POST", '/playlists/' . $this->request->get('myPlaylist') . '/tracks?uris=' . $this->request->get('song'));
+            $this->view->myPlaylist = $result;
+            $this->response->redirect('index/myPlaylist?myPlaylist=' . $this->request->get('myPlaylist'));
+        }
     }
 
     private function topResults($items)
@@ -60,7 +103,7 @@ class IndexController extends Controller
         return $top_result;
     }
 
-    public function find($method, $query = '', $playlist = '')
+    public function find($method, $query = '', $body = '')
     {
         $client = new \GuzzleHttp\Client();
         if ($method == 'GET') {
@@ -80,18 +123,26 @@ class IndexController extends Controller
                         'Authorization' => "Bearer " . $this->config->get('app')['bearer_token'],
                     ],
                     'body' => json_encode(
-                        [
-                            "name" => $playlist,
-                            "description" => "New playlist description",
-                            "public" => false
-                        ]
+                        $body
                     )
                 ],
             );
         }
-        // $response = $client->post($this->config->get('url')['base_url'] . $query . $slug . $format, ['data' => 
-        // )]);
 
+        if ($method == "DELETE") {
+            $response = $client->request(
+                'DELETE',
+                $this->config->get('url')['base_url'] . $query,
+                [
+                    'headers' => [
+                        'Authorization' => "Bearer " . $this->config->get('app')['bearer_token'],
+                    ],
+                    'body' => json_encode(
+                        $body
+                    )
+                ],
+            );
+        }
         return $response;
     }
 }
